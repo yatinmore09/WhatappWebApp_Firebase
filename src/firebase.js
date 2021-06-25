f251971741bd67e2fbd7d064736c8776b02189e7
yatinmore <yatinmore97@gmail.com>
2021-06-25 19:49:09 +0530

    new test3

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
 import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//GET Below Settings from Firebase > Project Overview > Settings > General > Your apps > Firebase SDK snippet > Config
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
-  };
+  apiKey: "AIzaSyBbk691xPkkFMGvJFml1FzXxzhLze2OUP0",
+  authDomain: "whatsapp-firebase-yt-34a3d.firebaseapp.com",
+  projectId: "whatsapp-firebase-yt-34a3d",
+  storageBucket: "whatsapp-firebase-yt-34a3d.appspot.com",
+  messagingSenderId: "1036082989125",
+  appId: "1:1036082989125:web:3b808862b77cee9ec27b2c",
+  measurementId: "G-WLZZ5EHL3D",
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const db = firebaseApp.firestore(); 
-  const auth = firebaseApp.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-  export {auth,provider};
-  export default db;
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
